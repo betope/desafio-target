ee6d53a2eff7eb951159ea7a5ed91d4371a848b0
Beto <betosantanna@outlook.com>
2023-04-15 12:08:29 -0300

    Todos os exercícios

--- script.js
+++ script.js
@@ -20,37 +20,37 @@
 
 /* Exercício 3 */
 // Gera um array com vendas aleatórias para cada dia do mês
-const numDias = 30;
-const faturamentoDiario = [];
+// const numDias = 30;
+// const faturamentoDiario = [];
 
-for (let i = 0; i < numDias; i++) {
-  const faturamento = Math.random() * 10000;
-  faturamentoDiario.push(faturamento);
-}
+// for (let i = 0; i < numDias; i++) {
+//   const faturamento = Math.random() * 10000;
+//   faturamentoDiario.push(faturamento);
+// }
 
-// Calcula a média de faturamento da mês
-let media = 0;
+// // Calcula a média de faturamento da mês
+// let media = 0;
 
-for(let i = 0; i < faturamentoDiario.length; i++) {
-    media += faturamentoDiario[i];
-}
-media /= faturamentoDiario.length;
+// for(let i = 0; i < faturamentoDiario.length; i++) {
+//     media += faturamentoDiario[i];
+// }
+// media /= faturamentoDiario.length;
 
-//Mostra o menor valor diário
-let menorValor = Math.min(...faturamentoDiario)
-console.log(`O menor valor de faturamento no mês foi: ${menorValor.toFixed(2)}`);
+// //Mostra o menor valor diário
+// let menorValor = Math.min(...faturamentoDiario)
+// console.log(`O menor valor de faturamento no mês foi: ${menorValor.toFixed(2)}`);
 
-//Mostra o maior valor diário
-let maiorValor = Math.max(...faturamentoDiario);
-console.log(`O maior valor de faturamento no mês foi: ${maiorValor.toFixed(2)}`);
+// //Mostra o maior valor diário
+// let maiorValor = Math.max(...faturamentoDiario);
+// console.log(`O maior valor de faturamento no mês foi: ${maiorValor.toFixed(2)}`);
 
 
-// Retorna os quantos dias no mês tiveram vendas maiores que a média
-let maiorqMedia = faturamentoDiario.filter(function(valor) {
-    if(valor > media)
-    return valor;
-});
-console.log(`A média é ${media.toFixed(2)} e houveram ${maiorqMedia.length} dias no mês em que as vendas foram maiores que a média`)
+// // Retorna os quantos dias no mês tiveram vendas maiores que a média
+// let maiorqMedia = faturamentoDiario.filter(function(valor) {
+//     if(valor > media)
+//     return valor;
+// });
+// console.log(`A média é ${media.toFixed(2)} e houveram ${maiorqMedia.length} dias no mês em que as vendas foram maiores que a média`)
 
 
 /* Exercício 4 */
