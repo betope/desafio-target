8ffe2755da1cb1c8bd2cb14c826395c6823c1d10
Beto <betosantanna@outlook.com>
2023-04-15 12:15:47 -0300

    Ex 4

--- script.js
+++ script.js
@@ -18,20 +18,5 @@ for (let totFilial of filiais) {
 // Adiciona o valor que cada filial representa em porcentagem do valor total
 for(let totFilial of filiais) {
     const porcentagem = totFilial.vendas / total * 100;
-    console.log(`${totFilial.filial}: ${porcentagem.toFixed(2)}%`)
+    console.log(`${totFilial.filial} representa: ${porcentagem.toFixed(2)}% do valor total`)
 }
-
-total = sp + rj + mg + es + os;
-
-let perSP = sp / total * 100;
-let perRJ = rj / total * 100;
-let perMG = mg / total * 100;
-let perES = es / total * 100;
-let perOS = os / total * 100;
-
-console.log(`O total mensal da distribuidora é ${total} 
-SP representa ${perSP.toFixed(2)}%. 
-RJ representa ${perRJ.toFixed(2)}%
-MG representa ${perMG.toFixed(2)}%
-ES representa ${perES.toFixed(2)}%
-OS representa ${perOS.toFixed(2)}%`);
